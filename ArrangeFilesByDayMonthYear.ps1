d8bc1576629e69c80a6f83ee992075d7bae0cf35
jerrin francis <jerrinfrancis7@gmail.com>
2019-11-09 17:48:06 +0530

    Completing arranging files by date

--- ArrangeFilesByDayMonthYear.ps1
+++ ArrangeFilesByDayMonthYear.ps1
@@ -1,18 +1,28 @@
-param([string] $filepath = "C:\Users\I301970\Google Drive\Dreams\Cloud Engineering and ML Wizard\Powershell Tutorials")
-$folders = Get-ChildItem -Path $filepath -Recurse -Directory 
-$ItemTOMove = @()
+param([string] $filepath = "C:\Users\jerri\Documents\Test" ,
+    [string] $skippathpattern = ".git")
+$folders = Get-ChildItem -Path $filepath -Recurse -Directory | ? { $_.Parent.FullName -inotmatch $skippathpattern -and $_.FullName -inotmatch $skippathpattern }
 foreach ($folder in $folders) {
-    $items = get-Childitem -Path $folder.FullName -Recurse 
+    $items = get-Childitem -Path $folder.FullName -Recurse | Where-Object { $_.FullName -inotmatch $skippathpattern } 
     foreach ($item in $items) {
         if (( Get-Item $item.FullName ) -ne [System.IO.DirectoryInfo]) {
-            $path = $filepath + "\" + $item.CreationTime.Month
-            $ItemTOMove += @(($item.CreationTime.Month,$path))
+            $pathtomove = $filepath + "\" + $item.CreationTime.Year + "-" + $item.CreationTime.Month
+            $ItemTOMove += @(, @($item.CreationTime.Month, $pathtomove, $item.FullName))
         }        
     }
 }
-$ItemTOMove
+$targetfolderitems = get-Childitem -Path $filepath -File | Where-Object { $_.FullName -inotmatch $skippathpattern } 
+foreach ($item in $targetfolderitems) {
+    if (( Get-Item $item.FullName ) -ne [System.IO.DirectoryInfo]) {
+        $pathtomove = $filepath + "\" + $item.CreationTime.Year + "-" + $item.CreationTime.Month
+        $ItemTOMove += @(, @($item.CreationTime.Month, $pathtomove, $item.FullName))
+    }        
+}
+$ItemTOMove.Count
 for ($i = 0; $i -lt $ItemToMove.Count; $i++) {
-$ItemTOMove[$i][1]    
+    If (!(test-path $ItemTOMove[$i][1])) {
+        New-Item -ItemType Directory -Force -Path $ItemTOMove[$i][1]
+    }
+    Move-Item -Path $ItemTOMove[$i][2] -Destination $ItemTOMove[$i][1] -Force   
 }
 
 
