2574cfb173a606d75278b61b7b70dfcea95cc556
Francis <jerrin.francis@sap.com>
2019-11-13 17:12:53 +0530

    PrefixAllFiles with a prefix value recursevely

---
+++ PrefixAllFiles.ps1
@@ -0,0 +1,18 @@
+param([string] $Prefix = 'DUMMY',
+    [string] $FilePath = 'FILE_PATH')
+if (($FilePath -eq "FILE_PATH") -or (!(test-path $FilePath)) ) {
+    Write-Error "Provide a valid path!!!"
+    Return
+}
+if ($Prefix -eq "DUMMY") {
+    Write-Error "Prefix is mandatory"
+    Return
+    
+}
+$FilesToRename = Get-ChildItem -Path $FilePath -File -Recurse 
+foreach ($File in $FilesToRename) {
+    [string] $CurrentName = $File.Name
+    $PrefixedName = $Prefix + '' + $CurrentName
+    Rename-Item -Path $File.FullName -NewName $PrefixedName -Force
+    
+}
