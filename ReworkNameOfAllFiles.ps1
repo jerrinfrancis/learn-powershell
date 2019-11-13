85e8f939e26b6bce0681a6eb87de87501a8d5169
Francis <jerrin.francis@sap.com>
2019-11-13 10:24:15 +0530

    Replace all the files with searchstring recursevily and rename files with a replacement string

--- ReworkNameOfAllFiles.ps1
+++ ReworkNameOfAllFiles.ps1
@@ -1,11 +1,20 @@
-param([string] $ReplaceString = 'Useful',
-            [string] $path = '.')
-$ReplacePattern = '*' + $ReplaceString + '*'
+param([string] $SearchString = 'DUMMY' ,
+    [string] $ReplacementString = '',
+    [string] $FilePath = 'FILE_PATH')
+if (($FilePath -eq "FILE_PATH") -or (!(test-path $FilePath)) ) {
+    Write-Error "Provide a valid path!!!"
+    Return
+}
+if ($SearchString -eq "DUMMY") {
+    Write-Error "SearchString Parameter is Mandatory"
+    Return
+    
+}
+$ReplacePattern = '*' + $SearchString + '*'
 $FilesToRename = Get-ChildItem -Path $path -File -Recurse -Force -Filter $ReplacePattern
 foreach ($File in $FilesToRename) {
     [string] $CurrentName = $File.Name
-    $ReplaceName = $CurrentName.Replace($ReplaceString, '')
-    $ReplaceName
-    # Rename-Item -Path $File.FullName -NewName $ReplaceName
+    $ReplaceName = $CurrentName.Replace($SearchString, $Replacement)
+    Rename-Item -Path $File.FullName -NewName $ReplaceName -Force
     
 }
